0a97b3a74becccec804b90d720590898782dee9a
paulamacedo <paulamacedo@users.noreply.github.com>
2016-07-18 23:58:43 -0300

    Update func.c

--- func.c
+++ func.c
@@ -1,6 +1,7 @@
 #include "func.h"
 
-int acha_quant_linhas(FILE *entrada){
+int acha_quant_linhas(FILE* entrada){
+
 
 	int contador = 0 ;
 	int caractere, existe_linhas = 0;
@@ -20,39 +21,45 @@ int acha_quant_linhas(FILE *entrada){
 	return contador;//retorna o número de linhas do arquivo
 }
 
-PRODUTO armazena_dados(FILE *entrada, FILE *entrada2, PRODUTO produtos, int tamanho){
+void armazena_dados(FILE* entrada, FILE* entrada2, PRODUTO *produtos, int tamanho){
+	
 	int i;
-
-	for(i=0; i<tamanho; i++){
+	for(i=0; i< tamanho; i++){
 		fscanf(entrada, "%d %s", &produtos[i].codigo, produtos[i].descricao);
+		printf("PASSOU POR AQUI i %d %d %s\n ", i, produtos[i].codigo, produtos[i].descricao);
 	}
-	for(i=0; i<tamanho; i++){
+	for(i=0; i< tamanho; i++){
 		fscanf(entrada2, "%d %f", &produtos[i].codigo, &produtos[i].preco_unitario);
+		printf("PASSOU POR AQUI i %d %d %f\n", i, produtos[i].codigo,  produtos[i].preco_unitario);
 	}
-
-	return produtos;
 }
 
-int busca_produtos(PRODUTO produtos, int tamanho, int codigo){
+int busca_produtos(PRODUTO *produtos, int tamanho, int cod){
 
-	int direita, esquerda, meio;
+	int direita, esquerda, i=0;
 	esquerda = 0;
 	direita = tamanho-1;
 	while(esquerda <= direita) {
-		meio = (esquerda+direita)/2;
-		if(produtos[meio] == codigo) return meio;
-		if(codigo > produtos[meio]) esquerda = meio+1;
-		else direita = meio-1;
+		i = (esquerda+direita)/2;
+		if(produtos[i].codigo == cod){ 
+			return i;
+		}
+		if(cod > produtos[i].codigo) {
+			esquerda = i+1;
+		}
+		else {
+			direita = i-1;
+		}	
 	}
 	return -1;
 
 }
 
-float calcula_preços(PRODUTO produtos, int posicao, int quantidade){
+float calcula_precos(PRODUTO *produtos, int posicao, int quantidade){
 
 	float valor;
 
-	 valor = podutos[posicao].preço_unitario * quantidade; 
+	valor = produtos[posicao].preco_unitario * quantidade; 
 
 	return valor; 
 	
