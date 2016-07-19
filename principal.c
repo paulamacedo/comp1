700e4ba79da6ba0a18a8f6ba7d572998889cc486
paulamacedo <paulamacedo@users.noreply.github.com>
2016-07-18 23:57:41 -0300

    Update principal.c

--- principal.c
+++ principal.c
@@ -8,18 +8,19 @@ int main(){
 
 	//variáveis de arquivo 
 	FILE *entrada, *entrada2, *saida;
-	char nome_entrada[MAX], nome_entrada2[MAX], nome_saida[MAX];
-	//
+	char nome_entrada[MAX], nome_entrada2[MAX];
+
 	//variáveis de estrutura
 	PRODUTO *produtos;
-	// 
+ 
 	//variáveis de controle do menu
-	int cod, posicao, quantidade, opcao =0; 
-	//
+	int cod, posicao, quantidade, opcao = 1; 
+
  
 	//Interação com o usuário 1 - Solicitação de arquivo de entrada 1
 	puts("Por favor, entre com o nome do arquivo 1 (codigo e descrição)");	
 	scanf("%s", nome_entrada);
+
 	//Abertura de arquivo para leitura e teste
 	entrada = fopen(nome_entrada, "r");
 	if(!entrada){
@@ -43,8 +44,8 @@ int main(){
 	scanf("%s", nome_entrada2);
 	//
 	//Abertura de arquivo para leitura e teste
-	entrada = fopen(nome_entrada2, "r"); 
-	if(!entrada){
+	entrada2 = fopen(nome_entrada2, "r"); 
+	if(!entrada2){
 		puts("Erro! Não foi possível realizar a leitura do arquivo.");
 		return 1;	
 	}
@@ -52,27 +53,28 @@ int main(){
 	armazena_dados(entrada, entrada2, produtos, tamanho_arq);
 	//
 	//Gera um arquivo para escrita
-	saida = fopen(nome_saida, "w");
+	saida = fopen("lista.csv", "w");
 	if(!saida){
 		puts("Erro! Arquivo de saida não pode ser gerado.");
 		return 1;	
 	}
-	//Interação com o usuário 3 - MENU
 
+	//Interação com o usuário 3 - MENU
 	puts("Vamos começar a nossa busca?"); 
 	while(opcao!= 0){		
 			puts("Entre com o código do produto e a quantidade ");
 			scanf("%d %d", &cod, &quantidade);
-			posicao = busca_produto(produtos, tamanho, cod);
+			posicao = busca_produtos(produtos, tamanho_arq, cod);
 			if(posicao < 0){
 				puts("Código não encontrado!");
 				continue;			
 			}
+			
 			float valor = calcula_precos(produtos, posicao, quantidade);	
-			//gera_csv(saida, produtos)//olhar essa função depois
+			
 			printf("Deseja continuar com a busca?\n"
 				" 1 . Para continuar\n"
-				" 0 . Para encerrar busca e gerar csv ");	
+				" 0 . Para encerrar busca e gerar csv \n");	
 			scanf("%d", &opcao);
 		}
 		
